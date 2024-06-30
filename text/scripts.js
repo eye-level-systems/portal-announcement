f8e320fcc07aa010bbcbb2a9c574c549ba6999e9
eye-level-systems <kenneth.chang@myeyelevel.com>
2024-06-30 16:11:33 +0900

    Update scripts.js

--- text/scripts.js
+++ text/scripts.js
@@ -26,4 +26,24 @@ document.addEventListener('DOMContentLoaded', function() {
         autoplay: true,
         autoplaySpeed: 3000
     });
+    
+    // Go to Top button functionality
+    var goToTopBtn = document.getElementById("goToTopBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            goToTopBtn.style.display = "block";
+        } else {
+            goToTopBtn.style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    goToTopBtn.addEventListener("click", function() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    });
 });
